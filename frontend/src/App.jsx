4d8a1b53d5ad27a6db2b93554bb11948990153dd
Shubham Tyagi <131750433+Shubhamt45054@users.noreply.github.com>
2025-03-29 13:06:14 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,47 @@
-import Navbar from "./components/Navbar";
+import React, { useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navabar.jsx'
 
-import HomePage from "./pages/HomePage";
-import SignUpPage from "./pages/SignUpPage";
-import LoginPage from "./pages/LoginPage";
-import SettingsPage from "./pages/SettingsPage";
-import ProfilePage from "./pages/ProfilePage";
+import HomePage from './pages/HomePage.jsx';
+import SignUpPage from './pages/SignUpPage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import SettingsPage from './pages/SettingPage.jsx';
+import ProfilePage from './pages/ProfilePage.jsx';
 
-import { Routes, Route, Navigate } from "react-router-dom";
-import { useAuthStore } from "./store/useAuthStore";
-import { useThemeStore } from "./store/useThemeStore";
-import { useEffect } from "react";
-
-import { Loader } from "lucide-react";
-import { Toaster } from "react-hot-toast";
+import { useAuthStore } from "./store/useAuthStore.js";
+import useThemeStore from './store/useThemeStore.js';
+import {Loader} from "lucide-react";
+import { Navigate } from 'react-router';
+import { Toaster } from 'react-hot-toast';
+import ErrorBoundary from './utils/ErrorBoundarirs.jsx';
 
 const App = () => {
-  const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
-  const { theme } = useThemeStore();
 
-  console.log({ onlineUsers });
+  const {authUser, isCheckingAuth,checkAuth,onlineUsers} = useAuthStore();
+  const {theme, setTheme} = useThemeStore();
 
-  useEffect(() => {
+  console.log("Users");
+  console.log({onlineUsers});
+
+  useEffect(()=>{
     checkAuth();
-  }, [checkAuth]);
+  },[checkAuth]);
+  
+  console.log({authUser});
 
-  console.log({ authUser });
-
-  if (isCheckingAuth && !authUser)
+  if(isCheckingAuth && !authUser){
     return (
       <div className="flex items-center justify-center h-screen">
-        <Loader className="size-10 animate-spin" />
-      </div>
-    );
+      <Loader className="size-10 animate-spin" />
+    </div>
+    )
+  }
 
   return (
-    <div data-theme={theme}>
-      <Navbar />
+    <div data-theme={theme} >
+      <ErrorBoundary>
 
+      <Navabar/>
       <Routes>
         <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
         <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
@@ -44,9 +49,11 @@ const App = () => {
         <Route path="/settings" element={<SettingsPage />} />
         <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
+      <Toaster/>
 
-      <Toaster />
+      </ErrorBoundary>
     </div>
-  );
-};
+  )
+}
+
 export default App;
